8c6e2062205554b138f6b50de953a5bffbb2e61e
Karol Hernandez Castro <106127324+Karolhercastro@users.noreply.github.com>
2022-11-08 21:50:47 -0500

    firebase

--- app/src/main/java/com/example/muchofood/view/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/muchofood/view/ui/activities/LoginActivity.kt
@@ -4,22 +4,36 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.example.muchofood.R
+import com.google.android.gms.tasks.Task
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     lateinit var inicioboton:Button
     lateinit var registroboton:Button
     lateinit var recuperarcontra: TextView
+    lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        firebaseAuth=Firebase.auth
+        val correos= findViewById<EditText>(R.id.correo)
+        val contraseñaregis=findViewById<EditText>(R.id.contrasena)
+
+
         inicioboton = findViewById(R.id.recuperarBoton)
         registroboton= findViewById(R.id.registroLogin)
         recuperarcontra= findViewById(R.id.recuperarContra)
 
         inicioboton.setOnClickListener {
-            startActivity(Intent(this,HomeActivity::class.java))
+            user_login (correos.text.toString(),contraseñaregis.text.toString())
         }
 
         registroboton.setOnClickListener {
@@ -30,4 +44,15 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, RecuperarActivity::class.java))
         }
     }
+    fun user_login(correos:String, contraseñaregis:String){
+        firebaseAuth.signInWithEmailAndPassword(correos, contraseñaregis)
+            .addOnCompleteListener(this){
+                    Task-> if(Task.isSuccessful){
+                startActivity(Intent(this,HomeActivity::class.java))
+            }else{
+                Toast.makeText(baseContext,"ERROR",Toast.LENGTH_LONG).show()
+            }
+            }
+    }
+
 }
--- app/src/main/java/com/example/muchofood/view/ui/activities/RegistroActivity.kt
+++ app/src/main/java/com/example/muchofood/view/ui/activities/RegistroActivity.kt
@@ -4,10 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.muchofood.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class RegistroActivity : AppCompatActivity() {
     lateinit var registrohomeboton: Button
+    lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
@@ -15,6 +22,34 @@ class RegistroActivity : AppCompatActivity() {
 
         registrohomeboton.setOnClickListener {
             startActivity(Intent(this,HomeActivity::class.java))
+
         }
+
+        firebaseAuth=Firebase.auth
+        val nombres = findViewById<EditText>(R.id.nombreRegistro)
+        val apellidos= findViewById<EditText>(R.id.apellidosRegistro2)
+        val correos= findViewById<EditText>(R.id.correoRegistro2)
+        val celulares= findViewById<EditText>(R.id.celularRegistro2)
+        val direccion= findViewById<EditText>(R.id.direccionRegistro2)
+        val observacion= findViewById<EditText>(R.id.observacionesRegistro)
+        val contraseñaregis= findViewById<EditText>(R.id.contrasenaRegistro2)
+        val contraseñaConfirmar= findViewById<EditText>(R.id.contrasenaConfirmarRegistro)
+
+
+        registrohomeboton=findViewById(R.id.ingresarRegistro2)
+        registrohomeboton.setOnClickListener {
+            createUser(correos.text.toString(),contraseñaregis.text.toString())
+        }
+    }
+
+    fun createUser(correos:String, contraseñaregis:String){
+        firebaseAuth.createUserWithEmailAndPassword(correos,contraseñaregis)
+            .addOnCompleteListener(this){
+                Task->if(Task.isSuccessful){
+                startActivity(Intent(this,LoginActivity::class.java))
+                }else{
+                    Toast.makeText(baseContext,"intente de nuevo",Toast.LENGTH_LONG).show()
+            }
+            }
     }
 }
